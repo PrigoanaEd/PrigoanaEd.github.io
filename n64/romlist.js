9a80b2e9157a6b8ad8f659d8723137e734d2d9ed
Eduard Prigoana <146183093+PrigoanaEd@users.noreply.github.com>
2024-05-21 21:19:13 +0300

    Add files via upload

---
+++ n64/romlist.js
@@ -0,0 +1,18 @@
+var ROMLIST = [
+    {url:"https://f-e-m-b-o-y.github.io/roms/1",title:"Banjo-Kazooie"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/2",title:"Banjo-Tooie"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/3",title:"Castlevania"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/4",title:"Conkers Bad Fur Day"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/5",title:"Donkey Kong 64"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/6",title:"Doom 64"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/7",title:"Golden Nugget 64"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/8",title:"GoldenEye 007"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/9",title:"Paper Mario"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/10",title:"Pyoro"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/11",title:"South Park"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/12",title:"Star Fox 64"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/13",title:"Super Mario 64"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/14",title:"Super Smash Bros"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/15",title:"The Legend of Zelda: Majora's Mask"},
+    {url:"https://f-e-m-b-o-y.github.io/roms/16",title:"The Legend of Zelda: Ocarina of Time"}
+];
