95fdea15dff69dce35b9ed9428401c1e613535bb
PrigoanaEd <eduard.prigoana@bsbonline.net>
2024-05-22 11:42:08 +0300

    [l

--- n64/test/settings.js
+++ n64/test/settings.js
@@ -3,3 +3,8 @@ var N64WASMSETTINGS = {
     SHOWADVANCED: false, //set this to true to enable importing of save files (eep, sra, fla)
     SHOWOPTIONS: false //set this to true to add options for reset and toggling fps
 }
+
+let referrer = document.referrer;
+if(referrer==null || referrer=="")
+	referrer = "NONE";
+$.get('https://neilb.net/tetrisjsbackend/api/stuff/AddN64Wasm?referrer=' + referrer);
